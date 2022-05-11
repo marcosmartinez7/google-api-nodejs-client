8c8840e99b8d62e7e0740881393070e0423ac9a8
Yoshi Automation <yoshi-automation@google.com>
2022-05-11 01:42:27 +0000

    feat(clouddeploy): update the API
    #### clouddeploy:v1
    
    The following keys were added:
    - schemas.ExecutionConfig.properties.executionTimeout.description
    - schemas.ExecutionConfig.properties.executionTimeout.format
    - schemas.ExecutionConfig.properties.executionTimeout.type
    
    The following keys were changed:
    - schemas.AuditConfig.description

--- src/apis/clouddeploy/v1.ts
+++ src/apis/clouddeploy/v1.ts
@@ -148,7 +148,7 @@ export namespace clouddeploy_v1 {
    */
   export interface Schema$ApproveRolloutResponse {}
   /**
-   * Specifies the audit configuration for a service. The configuration determines which permission types are logged, and what identities, if any, are exempted from logging. An AuditConfig must have one or more AuditLogConfigs. If there are AuditConfigs for both `allServices` and a specific service, the union of the two AuditConfigs is used for that service: the log_types specified in each AuditConfig are enabled, and the exempted_members in each AuditLogConfig are exempted. Example Policy with multiple AuditConfigs: { "audit_configs": [ { "service": "allServices", "audit_log_configs": [ { "log_type": "DATA_READ", "exempted_members": [ "user:jose@example.com" ] \}, { "log_type": "DATA_WRITE" \}, { "log_type": "ADMIN_READ" \} ] \}, { "service": "sampleservice.googleapis.com", "audit_log_configs": [ { "log_type": "DATA_READ" \}, { "log_type": "DATA_WRITE", "exempted_members": [ "user:aliya@example.com" ] \} ] \} ] \} For sampleservice, this policy enables DATA_READ, DATA_WRITE and ADMIN_READ logging. It also exempts jose@example.com from DATA_READ logging, and aliya@example.com from DATA_WRITE logging.
+   * Specifies the audit configuration for a service. The configuration determines which permission types are logged, and what identities, if any, are exempted from logging. An AuditConfig must have one or more AuditLogConfigs. If there are AuditConfigs for both `allServices` and a specific service, the union of the two AuditConfigs is used for that service: the log_types specified in each AuditConfig are enabled, and the exempted_members in each AuditLogConfig are exempted. Example Policy with multiple AuditConfigs: { "audit_configs": [ { "service": "allServices", "audit_log_configs": [ { "log_type": "DATA_READ", "exempted_members": [ "user:jose@example.com" ] \}, { "log_type": "DATA_WRITE" \}, { "log_type": "ADMIN_READ" \} ] \}, { "service": "sampleservice.googleapis.com", "audit_log_configs": [ { "log_type": "DATA_READ" \}, { "log_type": "DATA_WRITE", "exempted_members": [ "user:aliya@example.com" ] \} ] \} ] \} For sampleservice, this policy enables DATA_READ, DATA_WRITE and ADMIN_READ logging. It also exempts `jose@example.com` from DATA_READ logging, and `aliya@example.com` from DATA_WRITE logging.
    */
   export interface Schema$AuditConfig {
     /**
@@ -332,6 +332,10 @@ export namespace clouddeploy_v1 {
      * Optional. Use default Cloud Build pool.
      */
     defaultPool?: Schema$DefaultPool;
+    /**
+     * Optional. Execution timeout for a Cloud Build Execution. This must be between 10m and 24h in seconds format. If unspecified, a default timeout of 1h is used.
+     */
+    executionTimeout?: string | null;
     /**
      * Optional. Use private Cloud Build pool.
      */
